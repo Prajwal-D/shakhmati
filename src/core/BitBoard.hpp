44793ce528318bee72db6e43b771fcff76258e7a
Prajwal-D <prajwal.dethekar@gmail.com>
2023-06-02 22:57:52 +0100

    Added fullboard to bitboard

--- src/core/BitBoard.hpp
+++ src/core/BitBoard.hpp
@@ -6,7 +6,7 @@ typedef uint64_t BitBoard;
 
 struct AllBoards
 {
-    AllBoards()
+    constexpr AllBoards()
     :whiteKings(0ull), whiteQueens(0ull), whiteRooks(0ull), whiteBishops(0ull), whiteKnights(0ull), whitePawns(0ull),
     blackKings(0ull), blackQueens(0ull), blackRooks(0ull), blackBishops(0ull), blackKnights(0ull), blackPawns(0ull)
     {}
@@ -26,6 +26,20 @@ struct AllBoards
 
 };
 
+struct FullBoard
+{
+    FullBoard()
+    :curBoard(),castling(0x0F)
+    {};
+
+    AllBoards curBoard;
+    uint8_t castling; //initialised as 0000 1111, where top 4 bits are ignored
+                      //1111 goes left to right as white king-side, white queen-side, black king-side, black queen-side
+    
+
+
+    
+};
 
 
 
