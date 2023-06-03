e3d439509de39831ab025afe4bedcb22af29a270
Prajwal-D <prajwal.dethekar@gmail.com>
2023-06-03 23:49:51 +0100

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,22 @@ int main(void)
     */
     //Debug::print_encoded_move_str(Moves::encode_move(63,44,Magics::KNIGHT,false));
     //Debug::print_bb();
-    MoveGen::init_MoveGen();
-    Debug::print_bb(BitBoard(0x00'00'01'00'00'00'00'00));
-    Debug::print_bb(BitBoard(0x00'00'00'02'00'00'00'00));
-    MoveGen::BLACK_PIECES = 0x00'00'01'00'00'00'00'00;
-    Debug::print_bb(MoveGen::BLACK_PIECES);
-    std::cout << __builtin_ctzll(MoveGen::BLACK_PIECES) << std::endl;
+    // MoveGen::init_MoveGen();
+    // Debug::print_bb(BitBoard(0x00'00'01'00'00'00'00'00));
+    // Debug::print_bb(BitBoard(0x00'00'00'02'00'00'00'00));
+    // MoveGen::BLACK_PIECES = 0x00'00'01'00'00'00'00'00;
+    // Debug::print_bb(MoveGen::BLACK_PIECES);
+    // std::cout << __builtin_ctzll(MoveGen::BLACK_PIECES) << std::endl;
     
-    std::vector<Move> PossibleMoves = MoveGen::white_pawn_moves(BitBoard(0x00'00'00'02'00'00'00'00),BitBoard(0x00'00'01'00'00'00'00'00));
-    for (const Move& i : PossibleMoves)
-    {
-        Debug::print_encoded_move_str(i);
-    }
+    // std::vector<Move> PossibleMoves = MoveGen::white_pawn_moves(BitBoard(0x00'00'00'02'00'00'00'00),BitBoard(0x00'00'01'00'00'00'00'00));
+    // for (const Move& i : PossibleMoves)
+    // {
+    //     Debug::print_encoded_move_str(i);
+    // }
     
-
+    BoardState curState;
+    curState.fen_importer(STARTING_FEN);
+    Debug::print_entire_board(curState.curBoard);
     
     return 0;
 }
